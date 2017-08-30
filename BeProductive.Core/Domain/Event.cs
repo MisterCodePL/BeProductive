3a444865c7e5fabc3b31b1e1daec9e9387ea9ca4
MisterCodePL <mistercode11@gmail.com>
2017-08-30 18:20:36 +0200

    Added domain classes.

---
+++ BeProductive.Core/Domain/Event.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BeProductive.Core.Domain
+{
+    public class Event
+    {
+        public Guid Id { get; private set; }
+        public Guid OwnerId { get; private set; }
+        public string Name { get; private set; }
+        public DateTime StartAt { get; private set; }
+        public DateTime EndAt { get; private set; }
+        public string Description { get; private set; }
+
+
+        protected Event()
+        {
+        }
+
+        public Event(Guid ownerId, string name, DateTime startAt, DateTime endAt)
+        {
+            SetOwnerId(ownerId);
+            SetName(name);
+            SetStartAt(startAt);
+            SetEndAt(endAt);
+        }
+
+        private void SetOwnerId(Guid ownerId)
+        {
+            if (ownerId != null) OwnerId = ownerId;
+            else throw new NullReferenceException();
+        }
+
+        public void SetName(string name)
+        {
+            if (Name != null) Name = name;
+            else throw new NullReferenceException();
+        }
+
+        public void SetStartAt(DateTime startAt)
+        {
+            StartAt = startAt;
+        }
+
+        public void SetEndAt(DateTime endAt)
+        {
+            if (endAt == null) throw new NullReferenceException();
+            if (DateTime.Compare(StartAt, endAt) < 0) EndAt = endAt;
+            else throw new Exception("End time must be later than Start time.");
+        }
+    }
+}
---
+++ BeProductive.Core/Domain/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace BeProductive.Core.Domain
+{
+    class User
+    {
+        public Guid Id { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string Salt { get; private set; }
+        public string Username { get; private set; }
+        public string FullName { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public IEnumerable<Event> Events { get; private set; } 
+        
+        protected User()
+        {
+        }
+
+        public User(string email, string username, string password, string salt)
+        {
+            Id = Guid.NewGuid();
+            SetEmail(email);
+            SetUsername(username);
+            SetPassword(password, salt);
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void SetUsername(string username)
+        {
+            if (username != null) Username = username;
+            else throw new NullReferenceException();
+        }
+
+        public void SetEmail(string email)
+        {
+            var address = new MailAddress(email);
+            if (address != null) Email = email;
+            else throw new Exception("Email address is incorrect.");
+        }
+
+        public void SetPassword(string password, string salt)
+        {
+            if (password == null) throw new NullReferenceException();
+            if (password.Length >= 8)
+            {
+                Password = password;
+                Salt = salt;
+            }
+            else throw new Exception("Password is too short.");
+        }
+    }
+}
