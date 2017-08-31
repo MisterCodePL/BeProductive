483f3fa1c170f0fdcc6dd805fdd4394b7f3bf3e5
MisterCodePL <mistercode11@gmail.com>
2017-08-31 16:26:06 +0200

    Added UserDto.

---
+++ BeProductive.Infrastructure/DTO/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using BeProductive.Core.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace BeProductive.Infrastructure.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public IEnumerable<Event> Events { get; set; }
+    }
+}
