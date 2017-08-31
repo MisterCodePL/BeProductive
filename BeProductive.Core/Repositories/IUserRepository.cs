961daaf9a6366f15bd2a6fb2f1bb907afb81e619
MisterCodePL <mistercode11@gmail.com>
2017-08-31 14:54:45 +0200

    Added IUserRepository.

---
+++ BeProductive.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using BeProductive.Core.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace BeProductive.Core.Repositories
+{
+    interface IUserRepository
+    {
+        User Get(string email);
+        User Get(Guid id);
+        IEnumerable<User> GetAll();
+        void Add(User user);
+        void Remove(Guid id);
+        void Update(User user);
+    }
+}
