1d86aa7196f282decc89d894b751c2c4955b5603
MisterCodePL <mistercode11@gmail.com>
2017-09-05 20:57:25 +0200

    Added requirements of user fields.

--- BeProductive.Core/Domain/User.cs
+++ BeProductive.Core/Domain/User.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Mail;
+using System.Text.RegularExpressions;
 
 namespace BeProductive.Core.Domain
 {
     public class User
     {
+        private static readonly Regex PasswordRegex = new Regex("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{4,20}$");
+        private static readonly Regex UsernameRegex = new Regex("^[A-Za-z0-9_-]{3,15}$");
+        private static readonly Regex FullNameRegex = new Regex("^[^@#$%^&*!]+$");
+
         public Guid Id { get; protected set; }
         public string Email { get; protected set; }
         public string Password { get; protected set; }
@@ -13,6 +18,7 @@ namespace BeProductive.Core.Domain
         public string Username { get; protected set; }
         public string FullName { get; protected set; }
         public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
 
         public IEnumerable<Event> Events { get; protected set; } 
         
@@ -27,30 +33,57 @@ namespace BeProductive.Core.Domain
             SetUsername(username);
             SetPassword(password, salt);
             CreatedAt = DateTime.UtcNow;
+            UpdateStatus();
         }
 
         public void SetUsername(string username)
         {
-            if (username != null) Username = username;
-            else throw new NullReferenceException();
+            if (username == null) throw new ArgumentNullException();
+            if (Username == username) return;
+            if (username.Length < 4) throw new Exception("Username is shorter than 3 chars.");
+            if (username.Length > 20) throw new Exception("Username is longer than 15 chars.");
+            if (!UsernameRegex.IsMatch(username))
+                throw new Exception("Username contains inappropriate characters.");
+            Username = username;
+            UpdateStatus();
         }
 
         public void SetEmail(string email)
         {
+            if (email == null) throw new ArgumentNullException();
+            if (Email == email.ToLowerInvariant()) return;
             var address = new MailAddress(email);
-            if (address != null) Email = email.ToLowerInvariant();
-            else throw new Exception("Email address is incorrect.");
+            if (address != null) throw new Exception("Email address is incorrect.");
+            Email = email.ToLowerInvariant();
+            UpdateStatus();
         }
 
         public void SetPassword(string password, string salt)
         {
-            if (password == null) throw new NullReferenceException();
-            if (password.Length >= 8)
-            {
-                Password = password;
-                Salt = salt;
-            }
-            else throw new Exception("Password is too short.");
+            if (password == null) throw new ArgumentNullException();
+            if (Password == password) return;
+            if (password.Length < 4) throw new Exception("Password is shorter than 4 chars.");
+            if (password.Length > 20) throw new Exception("Password is longer than 20 chars.");
+            if (!PasswordRegex.IsMatch(password))
+                throw new Exception("Password do not have one big letter, one small letter or one digit.");
+            Password = password;
+            Salt = salt;
+            UpdateStatus();
+        }
+
+        public void SetFullName(string fullName)
+        {
+            if (fullName == null) throw new ArgumentNullException();
+            if (FullName == fullName) return;
+            if (!FullNameRegex.IsMatch(fullName))
+                throw new Exception("FullName contains inappropriate characters.");
+            FullName = fullName;
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            UpdatedAt = DateTime.UtcNow;
         }
     }
 }
